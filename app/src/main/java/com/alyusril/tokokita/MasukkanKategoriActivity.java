f0b98539fdd0906b2999791793fb85fe3c6e0f00
radityabayuramadhan <radityabayuramadhan01@gmail.com>
2021-08-30 09:44:44 +0700

    radityaheadhunter

--- app/src/main/java/com/alyusril/tokokita/MasukkanKategoriActivity.java
+++ app/src/main/java/com/alyusril/tokokita/MasukkanKategoriActivity.java
@@ -4,22 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MasukkanKategoriActivity extends AppCompatActivity {
+    
+    EditText etSimpan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masukkan_kategori);
 
-
+        load();
+    }
+    public void load(){
+        etSimpan = findViewById(R.id.etNamaKategori);
     }
 
-
-    public void btnTambahKategori(View view) {
-        Intent intent = new Intent(MasukkanKategoriActivity.this, TambahKategoriActivity.class);
+    public void btnSimpan(View view) {
+        String simpan = etSimpan.getText().toString();
+        Intent intent = new Intent(this, TambahKategoriActivity.class);
+        intent.putExtra("ISI", simpan);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/alyusril/tokokita/TambahKategoriActivity.java
+++ app/src/main/java/com/alyusril/tokokita/TambahKategoriActivity.java
@@ -3,6 +3,8 @@ package com.alyusril.tokokita;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -10,22 +12,27 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class TambahKategoriActivity extends AppCompatActivity {
 
+    TextView tvMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_kategori);
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.fl_main, new KategoriFragment());
-        ft.commit();
+        load();
+        ambilData();
+    }
+    public void load(){
+        tvMenu = findViewById(R.id.tvMenuJualan);
+    }
 
+    public void ambilData(){
+        String ambil = getIntent().getStringExtra("ISI");
+        tvMenu.setText(ambil);
     }
 
     public void btnTambahKategori2(View view) {
         Intent intent = new Intent(TambahKategoriActivity.this, MasukkanKategoriActivity.class);
         startActivity(intent);
     }
-
-
 }
